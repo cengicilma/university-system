71665b7908eec81c7d9494769ee6949deab80fae
cengicilma <ilma.cengic@gmail.com>
2021-06-21 22:56:27 +0200

    composer update

---
+++ rest/index.php
@@ -0,0 +1,41 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH');
+require '../vendor/autoload.php';
+
+
+/**
+ * Username: 9ZM2evdyuU
+ * Database name: 9ZM2evdyuU
+ * Password: kF23TDnhot
+ * Server: remotemysql.com
+ * Port: 3306
+ */
+
+
+Flight::register('db', 'PDO', array('mysql:host=remotemysql.com;dbname=9ZM2evdyuU','9ZM2evdyuU','kF23TDnhot'));
+
+Flight::route('GET /users', function(){
+    $users = Flight::db()->query('SELECT * FROM users', PDO::FETCH_ASSOC)->fetchAll();
+    Flight::json($users);
+});
+
+Flight::route('GET /students', function(){
+    $students = Flight::db()->query('SELECT users.id, users.first_name, users.last_name, users.email, users.phone_number, users.address FROM users, students WHERE students.users_id = users.id', PDO::FETCH_ASSOC)->fetchAll();
+    Flight::json($students);
+});
+
+Flight::route('GET /professors', function(){
+    $professors = Flight::db()->query('SELECT users.id, users.first_name, users.last_name, users.email, users.phone_number, users.address FROM users, professors WHERE professors.user_id = users.id', PDO::FETCH_ASSOC)->fetchAll();
+    Flight::json($professors);
+});
+
+Flight::route('GET /courses', function(){
+    $courses = Flight::db()->query('SELECT * FROM courses', PDO::FETCH_ASSOC)->fetchAll();
+    Flight::json($courses);
+});
+
+
+
+Flight::start();
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '913643fb5437aaab90b8883855511b665fa00088',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '913643fb5437aaab90b8883855511b665fa00088',
+    ),
+    'mikecao/flight' => 
+    array (
+      'pretty_version' => 'v1.3.9',
+      'version' => '1.3.9.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bc5e03ca59accb3ef37e31b6d4a2a9c78c7bb7ce',
+    ),
+  ),
+);
